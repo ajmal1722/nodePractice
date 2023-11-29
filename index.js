d699178bc91cd2a27f52b022dfe9890777b26fc6
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-29 16:38:15 +0530

    readfilesync and writefilesync from lesson 5

--- index.js
+++ index.js
@@ -17,6 +17,9 @@
 // console.log('Hello ')
 // console.log('     World')
 
+
+// proacademy lesson : 4
+//*************************************** */
 // const readline = require('readline');
 // const rl = readline.createInterface({
 //     input : process.stdin,
@@ -31,16 +34,26 @@
 //     process.exit(0);
 // })
 
-const readline2 = require('readline');
-const rl2 = readline2.createInterface({
-    input : process.stdin,
-    output : process.stdout
-})
-rl2.question('How old are you ? ', (age) => {
-    console.log('your age is ' + age)
-    rl2.close();
-})
-rl2.on('close', () => {
-    console.log('exit')
-    process.exit(0);
-})
+// const readline2 = require('readline');
+// const rl2 = readline2.createInterface({
+//     input : process.stdin,
+//     output : process.stdout
+// })
+// rl2.question('How old are you ? ', (age) => {
+//     console.log('your age is ' + age)
+//     rl2.close();
+// })
+// rl2.on('close', () => {
+//     console.log('exit')
+//     process.exit(0);
+// })
+
+
+// Lesson 5
+
+const fs = require('fs');
+const textIn = fs.readFileSync('./files/input.txt', 'utf-8');
+console.log(textIn)
+
+const content = `data read from input.txt : ${textIn} \ndate created ${new Date()}`
+fs.writeFileSync('./files/output.txt', content);
