d4b027b85e0bad1296e01623da7870bfe8863286
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-29 12:41:33 +0530

    readline module take user input and disply output

--- index.js
+++ index.js
@@ -7,5 +7,26 @@
 // var cl = require('cli-color');
 // console.log(cl.red.underline('hello world'));
 
-const person = require('./person')
-console.log(person.name);
+// const person = require('./person')
+// console.log(person.name);
+
+// var message = ''
+// console.log(global.message);
+
+// console.log('Hello World')
+// console.log('Hello ')
+// console.log('     World')
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+rl.question('please enter your name : ', (name) => {
+    console.log('you are ' + name);
+    rl.close()
+})
+rl.on ('close', () => {
+    console.log('Interface is closed');
+    process.exit(0);
+})
