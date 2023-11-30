0dd4b445c10c33c171102b95d5373a0c5f50ba03
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-30 11:38:52 +0530

    created a html page and added it to the server

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-// var http = require('http');
-// http.createServer(function(req, res) {
-//     res.write('Hello from HTTP module');
-//     res.end();
-// }).listen('8080');
+const fs = require('fs');
+var http = require('http');
+
+const html = fs.readFileSync(`./Template/index.html`, 'utf-8');
+
+http.createServer(function(req, res) {
+    // Set the content type to HTML
+    res.setHeader('Content-Type', 'text/html');
+
+    // Write HTML content
+    res.write(html);
+
+    // End the response
+    res.end();
+}).listen(8080);
+
+
 
 // var cl = require('cli-color');
 // console.log(cl.red.underline('hello world'));
@@ -61,13 +73,13 @@
 // readfile from Lesson 7
 /******************************* */
 
-const fs = require('fs')
-fs.readFile('./files/start.txt', 'utf-8', (error, data) => {
-    console.log(data)
-    fs.readFile(`./files/${data}.txt`, `utf-8`, (error2, data2) => {
-        console.log(data2)
-        fs.writeFile('./files/output.txt', `${data2}\n\n${new Date()}`,(error3, data3) => {
-            console.log('file written succesfully');
-        })
-    })
-})
+// const fs = require('fs')
+// fs.readFile('./files/start.txt', 'utf-8', (error, data) => {
+//     console.log(data)
+//     fs.readFile(`./files/${data}.txt`, `utf-8`, (error2, data2) => {
+//         console.log(data2)
+//         fs.writeFile('./files/output.txt', `${data2}\n\n${new Date()}`,(error3, data3) => {
+//             console.log('file written succesfully');
+//         })
+//     })
+// })
